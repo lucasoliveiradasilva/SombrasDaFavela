eeea474c9117a9d6c71a3a272118c1765eeea52c
G7xiis <gabriellemosfreires@gmail.com>
2025-12-03 18:12:10 -0300

    hotbar pronta

--- Assets/Script/ItemPickup.cs
+++ Assets/Script/ItemPickup.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class ItemPickup : MonoBehaviour
 {
     public GameObject iconi;
-    
-   
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            iconi.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
 }
